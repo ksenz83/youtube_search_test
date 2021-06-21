b2d1a19f1150ec4850cf64d2d0e4d2e60b373696
Maksim Krasnov <krasnov_maksim@mail.ru>
2021-06-21 21:25:40 +0300

    Add readme.md and api key

--- youtube_search_test/settings.py
+++ youtube_search_test/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+YOUTUBE_API_KEY = 'AIzaSyAb1Mt4jlu5alfko92CCrUvQuRd1c37wno'
 
 try:
     from .local_settings import *
